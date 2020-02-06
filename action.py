403bd3a893f05aadda33f87ae3bd1562265a8a60
cccc <cuiwei_cv@163.com>
2020-02-06 13:37:35 +0800

    wakey wakey GitHub Actions

--- action.py
+++ action.py
@@ -2,8 +2,6 @@ import os
 from datetime import datetime
 from pytz import timezone
 
-# echo TZ="$INPUT_TZ1/$INPUT_TZ2"
-# echo "::set-output name=datetime::$(date +'%Y%m%d_%H%M%S')"
 print('time zone is {0:s}/{1:s}'.format(os.environ['INPUT_TZ1'], os.environ['INPUT_TZ2']))
 
 fmt = "%Y%m%d_%H%M%S_%Z"
@@ -11,7 +9,7 @@ now_utc = datetime.now(timezone('{0:s}/{1:s}'.format(os.environ['INPUT_TZ1'], os
 str_temp = now_utc.strftime(fmt)
 
 if None != str_temp:
-    command = 'echo "::set-output name=datetime_str::{0:s}"'.format(str_temp)
+    command = 'echo ::set-output name=datetime_str::{0:s}'.format(str_temp)
     print(command)
     print(os.popen(command).read())
 
