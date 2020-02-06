9297fb23e341710ecba4ef8d37429479dca14506
cccc <cuiwei_cv@163.com>
2020-02-06 12:01:31 +0800

    wakey wakey GitHub Actions

--- action.py
+++ action.py
@@ -11,7 +11,7 @@ now_utc = datetime.now(timezone('{0:s}/{1:s}'.format(os.environ['INPUT_TZ1'], os
 str_temp = now_utc.strftime(fmt)
 
 if None != str_temp:
-    command = 'echo "::set-output name=datetime::{0:s}"'.format(str_temp)
+    command = 'echo "::set-output name=datetime_str::{0:s}"'.format(str_temp)
     print(command)
     print(os.popen(command).read())
 
