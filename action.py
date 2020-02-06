90837a2b6b95a8eeb840284775958b1528614eec
cccc <cuiwei_cv@163.com>
2020-02-06 13:44:43 +0800

    wakey wakey GitHub Actions

--- action.py
+++ action.py
@@ -4,7 +4,7 @@ from pytz import timezone
 
 print('time zone is {0:s}/{1:s}'.format(os.environ['INPUT_TZ1'], os.environ['INPUT_TZ2']))
 
-fmt = "%Y%m%d_%H%M%S_%Z"
+fmt = "%Y%m%d_%H%M%S_%Z%z"
 now_utc = datetime.now(timezone('{0:s}/{1:s}'.format(os.environ['INPUT_TZ1'], os.environ['INPUT_TZ2'])))
 str_temp = now_utc.strftime(fmt)
 
