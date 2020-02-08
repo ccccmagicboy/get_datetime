670c61a4ad9830f2006a5867961e04605c2b8a2d
cccc <cuiwei_cv@163.com>
2020-02-08 11:21:39 +0800

    wakey wakey GitHub Actions

--- script.sh
+++ script.sh
@@ -9,15 +9,18 @@
 # echo "::set-output name=datetime::$(date +'%Y%m%d_%H%M%S')"
 
 if [ "$(uname)" == "Darwin" ]; then
-    # Do something under Mac OS X platform     
+    # Do something under Mac OS X platform    
+    echo "linux"
     brew install coreutils
     alias readlink=greadlink
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    # Do something under GNU/Linux platform
+    echo "linux"
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     # Do something under 32 bits Windows NT platform
+    echo "MINGW32_NT"
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
     # Do something under 64 bits Windows NT platform
+    echo "MINGW64_NT"
 fi
 
 # Absolute path to this script, e.g. /home/user/bin/foo.sh
